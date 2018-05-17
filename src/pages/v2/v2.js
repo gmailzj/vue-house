3897f802f94860ba54eb9b230938b712bdd73020
lake <gmailzj@qq.com>
2018-05-18 00:05:00 +0800

    hook lifecircle

--- src/pages/v2/v2.js
+++ src/pages/v2/v2.js
@@ -10,57 +10,56 @@ Vue.config.productionTip = false;
 Vue.config.performance = true;
 // 指定组件的渲染和观察期间未捕获错误的处理函数。这个处理函数被调用时，可获取错误信息和 Vue 实例
 Vue.config.errorHandler = function(err, vm, info) {
-    // handle error
-    // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
-    // 只在 2.2.0+ 可用
+  // handle error
+  // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+  // 只在 2.2.0+ 可用
 };
 
 // 为 Vue 的运行时警告赋予一个自定义处理函数。注意这只会在开发者环境下生效，在生产环境下它会被忽略。
 Vue.config.warnHandler = function(msg, vm, trace) {
-    // `trace` 是组件的继承关系追踪
+  // `trace` 是组件的继承关系追踪
 };
 
 // 须使 Vue 忽略在 Vue 之外的自定义元素 (e.g. 使用了 Web Components APIs)
 Vue.config.ignoredElements = [
-    "my-custom-web-component",
-    "another-web-component",
-    // 用一个 `RegExp` 忽略所有“ion-”开头的元素
-    // 仅在 2.5+ 支持
-    /^ion-/
+  "my-custom-web-component",
+  "another-web-component",
+  // 用一个 `RegExp` 忽略所有“ion-”开头的元素
+  // 仅在 2.5+ 支持
+  /^ion-/
 ];
 
 // 可以通过全局 config.keyCodes 对象自定义按键修饰符别名
 // 可以使用 `v-on:keyup.f1`
 Vue.config.keyCodes = {
-    v: 86,
-    f1: 112,
-    // camelCase 不可用
-    mediaPlayPause: 179,
-    // 取而代之的是 kebab-case 且用双引号括起来
-    "media-play-pause": 179,
-    up: [38, 87]
+  v: 86,
+  f1: 112,
+  // camelCase 不可用
+  mediaPlayPause: 179,
+  // 取而代之的是 kebab-case 且用双引号括起来
+  "media-play-pause": 179,
+  up: [38, 87]
 };
 Vue.config.keyCodes.f1 = 112;
 
 // 定义一个名为 button-counter 的新组件
 Vue.component("button-counter", {
-    data: function() {
-        return {
-            count: 0
-        };
-    },
-    template:
-        '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
 });
 
 // 定义全局组件
 Vue.component("mycomponent", {
-    template: `<div>这是一个自定义组件</div>`,
-    data() {
-        return {
-            message: "hello world"
-        };
-    }
+  template: `<div>这是一个自定义组件</div>`,
+  data() {
+    return {
+      message: "hello world"
+    };
+  }
 });
 
 /* eslint-disable no-new */
@@ -68,10 +67,36 @@ Vue.component("mycomponent", {
 
 /* eslint-disable no-new */
 new Vue({
-    el: "#app",
-    router,
-    template: "<App/>",
-    components: {
-        App
-    }
+  el: "#app",
+  router,
+  template: "<App/>",
+  components: {
+    App
+  },
+  beforeBreate: function() {
+    console.log("vm beforeBreate");
+  },
+  created: function() {
+    console.log("vm created");
+  },
+  beforMount: function() {
+    console.log("vm beforMount");
+  },
+  mounted: function() {
+    console.log("vm mounted");
+  },
+  beforeUpdate: function() {
+    console.log("vm beforeUpdate");
+  },
+  updated: function() {
+    console.log("vm updated");
+  },
+  beforeDestroy: function() {
+    // `this` 指向 vm 实例
+    console.log("vm beforeDestroy");
+  },
+  destroyed: function() {
+    // `this` 指向 vm 实例
+    console.log("vm destroyed");
+  }
 });
