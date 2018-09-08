b2332b9547f14e7b4b01fd566cb515913bd5b30d
gmailzj <gmailzj@qq.com>
2018-09-08 18:04:19 +0800

    vuex

--- src/pages/v2/store/index.js
+++ src/pages/v2/store/index.js
@@ -20,22 +20,47 @@ const vuexLocal = new VuexPersistence({
 // 如果在模块化构建系统中，请确保在开头调用了 Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
-        count: 0
+        count: 0,
+        region:null
     },
     mutations: {
-        increment(state, num) {
+        increment(state) {
           state.count++
         },
-        decrement(state, num) {
+        decrement(state) {
           state.count--
+        },
+        setRegion(state, payload) {
+          state.region = payload;
         }
+
     },
     actions:{
+      increment (context) {
+        context.commit('increment')
+      },
+      /* Action 函数接受一个与 store 实例具有相同方法和属性的 context 对象，因此你可以调用 context.commit 提交一个 mutation，
+          或者通过 context.state 和 context.getters 来获取 state 和 getters。当我们在之后介绍到 Modules 时，你就知道 context 对象为什么不是 store 实例本身了。
+          实践中，我们会经常用到 ES2015 的 参数解构 来简化代码（特别是我们需要调用 commit 很多次的时候）：
+      */
       test({
         commit,
         state
       }) {
         commit('increment')
+      },
+      getRegion({
+        commit,
+        state
+      }) {
+        if (!state.region) {
+            commit('SET_REGION', 'cn')
+        }
+      },
+    },
+    getters: {
+      region(state) {
+        return state.region || null
       }
     },
     // todo
